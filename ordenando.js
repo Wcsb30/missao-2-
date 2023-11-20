796a8f8e98df878f3d888ba00a03d93cb409c770
Wcsb30 <144966128+Wcsb30@users.noreply.github.com>
2023-11-20 14:44:25 -0300

    Add files via upload

---
+++ ordenando.js
@@ -0,0 +1,27 @@
+<html lang="pt-br">
+<head>
+  <title>Ordenando com JavaScript</title>
+</head>
+<body>
+  <input></input> type="number" id="valor"
+  <button onclick="add()">Adicionar</button>
+  <select id="algoritmo">
+    <option value="bubble">Bubble Sort</option>
+    <option value="selection">Selection Sort</option>
+    <option value="quick">Quick Sort</option>
+  
+</select>
+  <button onclick="ordenar()">Ordenar</button>
+  <button onclick="misturar()">Misturar</button>
+  
+ 
+<ul id="valores"></ul>
+
+  
+
+ 
+<script src="ordenando.js"></script>
+</body>
+
+/html
+</html>
